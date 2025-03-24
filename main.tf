65a0ffc62f5fa51c9e9042f92cff2ea16b6eb089
Lucy Davinhart <lucy@lucymhdavies.com>
2025-03-24 11:57:22 +0000

    Create an ARM EC2 instance we can use

--- main.tf
+++ main.tf
@@ -16,4 +16,111 @@ terraform {
 
 provider "aws" {
   region = "eu-west-2"
+
+  default_tags {
+    tags = {
+      Name      = "Test TFC Agents"
+      Terraform = "true"
+      Workspace = terraform.workspace
+      TTL       = "Ephemeral Workspace"
+      Owner     = "Lucy"
+      Purpose   = "Test TFC Agents"
+      Source    = "https://github.com/hashi-strawb/tf-agent-pool"
+    }
+  }
 }
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+data "aws_region" "current" {}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "agent-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs            = data.aws_availability_zones.available.names[*]
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+}
+
+
+# Allow us to easily connect to the EC2 instance with AWS EC2 Connect
+
+data "aws_ip_ranges" "ec2_instance_connect" {
+  regions  = [data.aws_region.current.name]
+  services = ["ec2_instance_connect"]
+}
+
+resource "aws_security_group" "ec2_instance_connect" {
+  name        = "ec2_instance_connect"
+  description = "Allow EC2 Instance Connect to access this host"
+
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port        = "22"
+    to_port          = "22"
+    protocol         = "tcp"
+    cidr_blocks      = data.aws_ip_ranges.ec2_instance_connect.cidr_blocks
+    ipv6_cidr_blocks = data.aws_ip_ranges.ec2_instance_connect.ipv6_cidr_blocks
+  }
+
+  tags = {
+    CreateDate = data.aws_ip_ranges.ec2_instance_connect.create_date
+    SyncToken  = data.aws_ip_ranges.ec2_instance_connect.sync_token
+  }
+}
+
+
+data "tfe_ip_ranges" "addresses" {}
+
+resource "aws_security_group" "outbound_http_tfc" {
+  name        = "outbound_http_tfc"
+  description = "Allow outbound HTTP access to TFC APIs"
+
+  vpc_id = module.vpc.vpc_id
+
+  egress {
+    from_port        = "443"
+    to_port          = "443"
+    protocol         = "tcp"
+    cidr_blocks      = data.tfe_ip_ranges.addresses.api
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+# Now create the EC2 instance
+resource "aws_instance" "agent" {
+  ami = data.aws_ami.amazon-linux-2.id
+
+  associate_public_ip_address = true
+
+  instance_type = "t4g.nano"
+  vpc_security_group_ids = [
+    aws_security_group.ec2_instance_connect.id,
+    aws_security_group.outbound_http_tfc.id,
+  ]
+
+  subnet_id = module.vpc.public_subnets[0]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
